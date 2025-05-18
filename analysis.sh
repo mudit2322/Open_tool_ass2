9814901c51d9e02947d2a3fc6cb667babba37808
mudit2322 <mudit.mamgain.mm@gmail.com>
2025-05-18 22:44:26 +0800

    chg1

--- analysis.sh
+++ analysis.sh
@@ -1,92 +1,95 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# analysis: Compute most popular mechanics/domains and Pearson correlations
+# Usage: analysis <cleaned-file>
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <cleaned-tsv-file>"
-    exit 1
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <cleaned-file>" >&2
+  exit 1
 fi
 
 file="$1"
 
-# Get column indices
-header=$(head -n 1 "$file")
-IFS=$'\t' read -ra cols <<< "$header"
-
-for i in "${!cols[@]}"; do
-    case "${cols[$i]}" in
-        "Mechanics") mech_col=$((i+1)) ;;
-        "Domains") domain_col=$((i+1)) ;;
-        "Year Published") year_col=$((i+1)) ;;
-        "Rating Average") rating_col=$((i+1)) ;;
-        "Complexity Average") complexity_col=$((i+1)) ;;
-    esac
-done
-
-# 1. Most popular Mechanics and Domain
-tail -n +2 "$file" | awk -F'\t' -v m="$mech_col" '
-{
-    n=split($m, arr, ",")
-    for (i=1; i<=n; i++) {
-        gsub(/^ +| +$/, "", arr[i])
-        if (arr[i] != "") count[arr[i]]++
+# Determine column indices from header
+read mech_col dom_col year_col rating_col comp_col < <(
+  awk 'BEGIN{FS="\t"} NR==1{
+    for(i=1;i<=NF;i++){
+      if($i=="Mechanics") m=i
+      if($i=="Domains") d=i
+      if($i=="Year Published" || $i=="Year") y=i
+      if($i=="Rating Average" || $i=="Average Rating") r=i
+      if($i=="Complexity Average" || $i=="Complexity") c=i
     }
+    print m, d, y, r, c
+    exit
+  }' "$file"
+)
+
+# Most popular mechanics
+awk -v FS="\t" -v col="$mech_col" '
+NR>1 {
+  n = split($col, arr, /,\s*/)
+  for(i=1;i<=n;i++) {
+    mech[arr[i]]++
+    if(mech[arr[i]] > max_count) {
+      max_count = mech[arr[i]]
+      max_mech = arr[i]
+    }
+  }
 }
 END {
-    max = 0
-    for (k in count) {
-        if (count[k] > max) {
-            max = count[k]
-            best = k
-        }
-    }
-    print "The most popular game mechanics is", best, "found in", max, "games"
-}'
+  printf "The most popular game mechanics is %s found in %d games\n", max_mech, max_count
+}' "$file"
 
-
-tail -n +2 "$file" | awk -F'\t' -v d="$domain_col" '
-{
-    n=split($d, arr, ",")
-    for (i=1; i<=n; i++) {
-        gsub(/^ +| +$/, "", arr[i])
-        if (arr[i] != "") count[arr[i]]++
+# Most popular domain
+awk -v FS="\t" -v col="$dom_col" '
+NR>1 {
+  n = split($col, arr, /,\s*/)
+  for(i=1;i<=n;i++) {
+    dom[arr[i]]++
+    if(dom[arr[i]] > max_count_dom) {
+      max_count_dom = dom[arr[i]]
+      max_dom = arr[i]
     }
+  }
 }
 END {
-    max = 0
-    for (k in count) {
-        if (count[k] > max) {
-            max = count[k]
-            best = k
-        }
-    }
-    print "The most game domain is", best, "found in", max, "games"
-}'
+  printf "The most popular game domain is %s found in %d games\n", max_dom, max_count_dom
+}' "$file"
 
-# 2. Correlation functions
-correlation() {
-    awk -F'\t' -v x="$1" -v y="$2" '
-    {
-        if (NR == 1) next
-        if ($x != "" && $y != "") {
-            xsum += $x
-            ysum += $y
-            x2sum += ($x)^2
-            y2sum += ($y)^2
-            xysum += ($x)*($y)
-            n++
-        }
-    }
-    END {
-        if (n > 0) {
-            numerator = n * xysum - xsum * ysum
-            denominator = sqrt((n * x2sum - xsum^2) * (n * y2sum - ysum^2))
-            corr = (denominator != 0) ? numerator / denominator : 0
-            printf "The correlation between %s and %s is %.3f\n", x == '$year_col' ? "the year of publication" : "the complexity of a game", "the average rating", corr
-        }
-    }'
+# Correlation: Year Published vs Rating Average
+awk -v FS="\t" -v yc="$year_col" -v rc="$rating_col" '
+NR>1 && $yc ~ /^[0-9]+$/ && $rc ~ /^[0-9]+(\.[0-9]+)?$/ {
+  x = $yc
+  y = $rc
+  n++
+  sumx += x
+  sumy += y
+  sumxy += x * y
+  sumx2 += x * x
+  sumy2 += y * y
 }
+END {
+  num = n * sumxy - sumx * sumy
+  den = sqrt((n * sumx2 - sumx^2) * (n * sumy2 - sumy^2))
+  r = (den > 0 ? num / den : 0)
+  printf "The correlation between the year of publication and the average rating is %.3f\n", r
+}' "$file"
 
-# Correlation 1: Year Published vs Rating Average
-correlation $year_col $rating_col
-
-# Correlation 2: Complexity Average vs Rating Average
-correlation $complexity_col $rating_col
+# Correlation: Complexity Average vs Rating Average
+awk -v FS="\t" -v cc="$comp_col" -v rc="$rating_col" '
+NR>1 && $cc ~ /^[0-9]+(\.[0-9]+)?$/ && $rc ~ /^[0-9]+(\.[0-9]+)?$/ {
+  x = $cc
+  y = $rc
+  n++
+  sumx += x
+  sumy += y
+  sumxy += x * y
+  sumx2 += x * x
+  sumy2 += y * y
+}
+END {
+  num = n * sumxy - sumx * sumy
+  den = sqrt((n * sumx2 - sumx^2) * (n * sumy2 - sumy^2))
+  r = (den > 0 ? num / den : 0)
+  printf "The correlation between the complexity of a game and its average rating is %.3f\n", r
+}' "$file"
